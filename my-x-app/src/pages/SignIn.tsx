a36b72aa80b8b9e43c9ce6a82fc89cd64397aff1
Jam-oji3 <shuei3150@gmail.com>
2025-06-12 12:08:50 +0900

    feat: 認証ページを追加

---
+++ my-x-app/src/pages/SignIn.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import SignInForm from '../features/auth/SignInForm';
+import styles from './SignIn.module.css';
+
+const SignIn: React.FC = () => {
+  return (
+    <div className={styles.page}>
+      <div className={styles.container}>
+        <SignInForm />
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
