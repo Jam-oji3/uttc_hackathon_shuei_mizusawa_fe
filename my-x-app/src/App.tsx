6f8cbe338248b5d2167b5a0cce5b899cfeb1332f
Jam-oji3 <shuei3150@gmail.com>
2025-06-14 19:38:47 +0900

    feat: React Routerの設定

--- my-x-app/src/App.tsx
+++ my-x-app/src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import './App.css';
 
 const App: React.FC = () => {
@@ -10,6 +11,7 @@ const App: React.FC = () => {
     <Routes>
       <Route path="/" element={<SignIn />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/sign-up" element={<SignUp />} />
     </Routes>
   );
 };
