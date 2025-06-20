574b4e2375cb3680c856605f06b8f8f7b2638668
Jam-oji3 <shuei3150@gmail.com>
2025-06-20 17:24:22 +0900

    feat: ルーティングを追加

--- my-x-app/src/App.tsx
+++ my-x-app/src/App.tsx
@@ -6,6 +6,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import UserProfile from './pages/UserProfile';
 import PostDetailPage from './pages/PostDetailPage';
+import NotificationPage from './pages/NotificationPage';
 import './App.css';
 
 const App: React.FC = () => {
@@ -14,8 +15,9 @@ const App: React.FC = () => {
       <Route path="/" element={<SignIn />} />
       <Route path="/home" element={<Home />} />
       <Route path="/sign-up" element={<SignUp />} />
-      <Route path="/user/:username" element={<UserProfile />} />
+      <Route path="/users/:username" element={<UserProfile />} />
       <Route path="/posts/:postId" element={<PostDetailPage />} />
+      <Route path="/notifications" element={<NotificationPage />} />
       {/* 他のルートもここに追加できます */}
 
     </Routes>
